716cf7a46c1a9addb769f60137da8aeb1a35d828
developlogy <easemyemail@gmail.com>
2025-06-21 13:06:02 +0530

    [+] Added Slogan In Seperate Files

--- src/sections/landing/Hero.jsx
+++ src/sections/landing/Hero.jsx
@@ -18,6 +18,7 @@ import { useTheme } from '@mui/material/styles';
 
 // project-imports
 import { techData } from 'data/tech-data';
+import { slogans, services } from '../../data/services';
 import { useIspValue } from 'hooks/useIspValue';
 import TechnologiesPage from './Technologies';
 
@@ -30,46 +31,6 @@ import AnimateButton from 'components/@extended/AnimateButton';
 export default function HeroPage() {
   const ispValueAvailable = useIspValue();
   const theme = useTheme();
-
-  const slogans = [
-    'Code smarter, dream bigger, achieve faster.',
-    'Elevate your brand - one search at a time.',
-    'Building credibility, one verified fact at a time.',
-    'Seamless IT solutions that keep your growth always online.'
-  ];
-
-  const services = [
-    {
-      title: 'SEO & Content',
-      description: 'Boost your website’s potential with targeted SEO strategies that improve rankings and drive traffic.',
-      image: '/images/services/seo.png'
-    },
-    {
-      title: 'Creative & Advertising',
-      description: 'Energize your brand with tailored social media strategies that create bold, lasting audience connections.',
-      image: '/images/services/social.png'
-    },
-    {
-      title: 'IT Solutions',
-      description: 'Create stunning, functional websites that captivate audiences and drive conversions effortlessly.',
-      image: '/images/services/it.png'
-    },
-    {
-      title: 'Software Development',
-      description: 'Leverage custom software that combines innovation and technical expertise to propel your business forward.',
-      image: '/images/services/software.png'
-    },
-    {
-      title: 'Wikipedia',
-      description: 'Create a lasting digital impression with Hats-Off Digital’s Wikipedia page services, tailored to showcase your story.',
-      image: '/images/services/wiki.png'
-    },
-    {
-      title: 'Cloud Solutions',
-      description: 'Experience seamless connectivity with our secure, flexible cloud services that streamline operations and drive innovation.',
-      image: '/images/services/cloud.png'
-    }
-  ];
 
   const [currentSlogan, setCurrentSlogan] = useState(0);
 
